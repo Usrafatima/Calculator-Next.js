7ac91e91bc6b7f0b1a745bc8639aba35cb9ed14d
Yusra Fatima <162156870+Usrafatima@users.noreply.github.com>
2024-09-20 22:43:58 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import CalculatorComponent from "@/components/calculator";
-import { Calculator } from "lucide-react";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
